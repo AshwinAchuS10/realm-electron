4ce31913252426b1d65e2f61d8bf42294b0bb49f
Ashwin S <achu10@live.in>
2023-05-26 15:21:48 +0530

    add auto update

--- public/electron.js
+++ public/electron.js
@@ -1,12 +1,18 @@
-const electron = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+
+const { autoUpdater } = require('electron-updater');
+
 const path = require('path');
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+// const app = electron.app;
+// const BrowserWindow = electron.BrowserWindow;
+// eslint-disable-next-line no-unused-vars
+let updateInterval = null;
 
 let mainWindow;
 
 function createWindow() {
+
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
@@ -21,4 +27,37 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  updateInterval = setInterval(() => autoUpdater.checkForUpdates(), 5000);
+});
+
+autoUpdater.on('update-available', (_event, releaseNotes, releaseName) => {
+  console.log('update-available*************');
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Ok'],
+    title: 'Update Available',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version download started. The app will be restarted to install the update.',
+  };
+  dialog.showMessageBox(dialogOpts);
+
+  updateInterval = null;
+});
+// win.loadURL("file:///" + __dirname + "/index.html");
+
+autoUpdater.on('update-downloaded', (_event, releaseNotes, releaseName) => {
+  console.log('update-downloaded*************');
+
+   const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+   };
+   dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+   });
+});
