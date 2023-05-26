729a623cdac404e30bfd2d6465b1487ff13f5341
Ashwin S <achu10@live.in>
2023-05-26 16:25:05 +0530

    incrment version

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import './index.css';
-const { BrowserWindow } = require('electron');
-const win = new BrowserWindow();
-win.webContents.openDevTools();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
